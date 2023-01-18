21f5c78f3a57ec7093ff937644a61ef267fc52ee
frodriguez <frodriguez@106-24.info2>
2023-01-18 14:38:50 +0100

    parte del 5

--- T5_Eliminacion/src/ejercicios/Ejercicio05.java
+++ T5_Eliminacion/src/ejercicios/Ejercicio05.java
@@ -1,9 +1,29 @@
 package ejercicios;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
+		int luckyNums;
+		int media;
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduzca la cantidad de números de la suerte que tiene: ");
+		luckyNums = sc.nextInt();
+		
+		int tabla[] = new int[luckyNums];
+		
+		for(int i=0; i<tabla.length; i++) {
+			System.out.print("Introduzca un número de la suerte: ");
+			tabla[i] = sc.nextInt();
+		} 
+		
+		Arrays.sort(tabla);
+		
+		sc.close();
 	}
 
 }
